88ed98e821acb051555492d6afdaf0d5e7e32054
Madina-12 <m03ghulam03@gmail.com>
2023-04-05 15:22:05 +0500

    url changed

--- features/api/apiSlice.js
+++ features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api', // optional
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://sherpur-sectionx.up.railway.app/' }),
     tagTypes: ['Post', 'Section'],
     endpoints: builder => ({})
 })
